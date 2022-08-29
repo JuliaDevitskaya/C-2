174ef5bba993aa656187c3c5d6da76d302d7276b
Julia <yagubetslulka@yandex.ru>
2022-08-29 03:36:29 +0300

    Задача1. Показать вторую цифру трехзначного числа

---
+++ 1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 10: 
+Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите трехзначное число: ");
+int number1 = Convert.ToInt32(ReadLine());
+if (number1 > 99 && number1 < 1000)
+{
+    int number2 = number1 / 10 % 10;
+    WriteLine(number2);
+}
+else
+{
+    WriteLine();
+}
